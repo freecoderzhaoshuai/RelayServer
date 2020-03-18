30cd5a164fbbf29351dcf6fe4a5e3477b1747044
ZHAO Shuai <roy.zhao@essilorchina.com>
2020-03-18 17:03:43 +0800

    Add project files.

---
+++ RelayServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Relay;
+using System.IO;
+using System.Threading;
+
+namespace RelayServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HybridConnectionListener listener = new HybridConnectionListener("Endpoint=sb://relayzs.servicebus.windows.net/;SharedAccessKeyName=sas;SharedAccessKey=WBhKG1Fb51sYz4I1Nsv/mZsLhnru+O08YQxeq+SyRfo=;EntityPath=hybirdconn1");
+            listener.OpenAsync();
+            Task<HybridConnectionStream> task =listener.AcceptConnectionAsync();
+            while (!task.IsCompleted) { }
+            HybridConnectionStream stream = task.Result;
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            String message=reader.ReadLine();
+            Console.WriteLine(message);
+            writer.WriteLine("Hello Client!");
+            writer.Flush();
+            reader.Close();
+            writer.Close();
+
+        }
+    }
+}
